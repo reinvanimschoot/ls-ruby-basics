508c494d64f102a0a5440a91c933d1288391f7ce
Rein Van Imschoot <reinvanimschoot@gmail.com>
2020-11-29 12:07:00 +0100

    Adds exercises for conditionals

---
+++ conditionals/5_truthy_number.rb
@@ -0,0 +1,18 @@
+=begin
+
+What will the following code print? Why? Don't run it until you've attempted to answer.
+
+number = 7
+
+if number
+  puts "My favorite number is #{number}."
+else
+  puts "I don't have a favorite number."
+end
+
+=end
+
+# It will print 'My favorite number is 7.'
+
+# The boolean value of an integer is truthy which means the if condition is true,
+# which will then subsequently print the string above.
---
+++ conditionals/8_sleep_alert.rb
@@ -0,0 +1,21 @@
+=begin
+
+In the code below, status is randomly assigned as 'awake' or 'tired'.
+
+status = ['awake', 'tired'].sample
+
+Write an if statement that returns "Be productive!" if status equals 'awake' 
+and returns "Go to sleep!" otherwise. 
+Then, assign the return value of the if statement to a variable and print that variable.
+
+=end
+
+status = ["awake", "tired"].sample
+
+suggestion = if status == "awake"
+    "Be productive!"
+  else
+    "Go to sleep!"
+  end
+
+puts suggestion
